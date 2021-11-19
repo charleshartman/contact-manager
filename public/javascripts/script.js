de78670a1abcee305ee5ea3cc5c124868de23bbd
Charles A. Hartman <charles@hartman.dev>
2021-11-19 07:53:36 -0800

    assessment prep JS239 project

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,20 +6,34 @@ class Model {
   constructor() {
     this.contacts;
     this.getAllContacts();
+    // this.getAllContacts().then(data => this.contacts = data);
   }
 
+  // async getAllContacts() {
+  //   await fetch('http://localhost:3000/api/contacts')
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       this.contacts = data;
+  //     })
+  //     .catch((error) => {
+  //       console.log('Error! Something went wrong...');
+  //       console.error(error);
+  //     });
+  // }
+
   async getAllContacts() {
-    await fetch('http://localhost:3000/api/contacts', { method: 'GET' })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        this.contacts = data;
-      })
-      .catch((error) => {
-        console.log('Error! Something went wrong...');
-        console.error(error);
-      });
+    try {
+      const endpointUrl = 'http://localhost:3000/api/contacts';
+      const response = await fetch(endpointUrl);
+      const json = await response.json();
+      this.contacts = json;
+      return json;
+    } catch (error) {
+      console.log('Error! Something went wrong...');
+      console.error(error);
+    }
   }
 }
 
