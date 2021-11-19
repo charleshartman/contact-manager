c4847c13a7193566f805b81e09f90e60ea45d1b8
Charles A. Hartman <charles@hartman.dev>
2021-11-18 20:14:42 -0800

    assessment prep JS239 project

---
+++ public/javascripts/script.js
@@ -0,0 +1,37 @@
+// script.js
+
+"use strict";
+
+class Model {
+  constructor() {
+    this.contacts = [];
+    this.getAllContacts();
+  }
+
+  async getAllContacts() {
+    await fetch('http://localhost:3000/api/contacts', { method: 'GET' })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.contacts = data;
+        return;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+}
+
+class View {
+  constructor() {}
+}
+
+class Controller {
+  constructor(model = new Model(), view = new View()) {
+    this.model = model;
+    this.view = view;
+  }
+}
+
+const app = new Controller();
