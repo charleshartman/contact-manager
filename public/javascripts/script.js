ce4974735bf3849a1389637ee146f610dbee6b46
Charles A. Hartman <charles@hartman.dev>
2021-11-20 13:51:54 -0800

    assessment prep JS239 project - model v0.1, working on view

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -109,7 +109,88 @@ class Model {
 
 
 class View {
-  constructor() {}
+  constructor() {
+    // root element container div
+    this.app = this.getElement('#root');
+
+    // title of app - h1
+    this.title = this.createElement('h1');
+    this.title.textContent = 'Contact Manager';
+
+    // search input field (no button, live search)
+    // one field to search full name OR tag name
+    this.search = this.createElement('input', 'search');
+    this.search.type = 'text';
+    this.search.placeholder = 'Search';
+
+    // add contact button, will show/hide contact form
+    this.upperAddContactButton = this.createElement('button');
+    this.upperAddContactButton.textContent = 'Add Contact';
+
+    // display entire list or filtered list
+    this.contactList = this.createElement('ul', 'contactList');
+
+    // add or edit form
+    this.form = this.createElement('form');
+
+    this.labelName = this.createElement('label');
+    this.labelName.setAttribute('for', 'name');
+    this.labelName.textContent = 'Full name:';
+    this.inputName = this.createElement('input');
+    this.inputName.name = 'name';
+    this.inputName.type = 'text';
+
+    this.labelEmail = this.createElement('label');
+    this.labelEmail.setAttribute('for', 'email');
+    this.labelEmail.textContent = 'Email address:';
+    this.inputEmail = this.createElement('input');
+    this.inputEmail.name = 'email';
+    this.inputEmail.type = 'text';
+
+    this.labelPhone = this.createElement('label');
+    this.labelPhone.setAttribute('for', 'phone');
+    this.labelPhone.textContent = 'Telephone number:';
+    this.inputPhone = this.createElement('input');
+    this.inputPhone.name = 'phone';
+    this.inputPhone.type = 'text';
+
+    this.labelTags = this.createElement('label');
+    this.labelTags.setAttribute('for', 'tags');
+    this.labelTags.textContent = 'Tags:';
+    this.inputTags = this.createElement('input');
+    this.inputTags.name = 'tags';
+    this.inputTags.type = 'text';
+
+    this.submitButton = this.createElement('button');
+    this.submitButton.textContent = 'Submit';
+
+    this.cancelButton = this.createElement('button');
+    this.cancelButton.textContent = 'Cancel';
+
+    this.form.append(this.labelName, this.inputName, this.labelEmail, this.inputEmail,
+      this.labelPhone, this.inputPhone, this.labelTags, this.inputTags,
+      this.submitButton, this.cancelButton);
+
+    this.app.append(this.title, this.search, this.upperAddContactButton,
+      this.contactList, this.form);
+  }
+
+  // Create element with optional class
+  createElement(tag, className) {
+    const element = document.createElement(tag);
+    if (className) {
+      element.classList.add(className);
+    }
+
+    return element;
+  }
+
+  // Retrieve an element from the DOM
+  getElement(selector) {
+    const element = document.querySelector(selector);
+
+    return element;
+  }
 }
 
 class Controller {
